52f73a84b99fcd5a8a52d9e3407049add1f63b21
sm277 <sm2774us@gmail.com>
2021-01-19 21:13:24 -0500

    Adding solutions for Facebook.

--- 001_Coding_Interview/LeetCode-Solutions/Python/next-permutation.py
+++ 001_Coding_Interview/LeetCode-Solutions/Python/next-permutation.py
@@ -2,6 +2,17 @@
 # Space: O(1)
 
 class Solution(object):
+    #
+    # According to Wikipedia: https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order,
+    # a man named Narayana Pandita presented the following simple algorithm to solve this problem
+    # in the 14th century.
+    #
+    # 1) Find the largest index k such that nums[k] < nums[k + 1]. If no such index exists,
+    #    just reverse nums and done.
+    # 2) Find the largest index l > k such that nums[k] < nums[l].
+    # 3) Swap nums[k] and nums[l].
+    # 4) Reverse the sub-array nums[k + 1:].
+    #
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
@@ -27,6 +38,17 @@ class Solution(object):
 # Time:  O(n)
 # Space: O(1)
 class Solution2(object):
+    #
+    # According to Wikipedia: https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order,
+    # a man named Narayana Pandita presented the following simple algorithm to solve this problem
+    # in the 14th century.
+    #
+    # 1) Find the largest index k such that nums[k] < nums[k + 1]. If no such index exists,
+    #    just reverse nums and done.
+    # 2) Find the largest index l > k such that nums[k] < nums[l].
+    # 3) Swap nums[k] and nums[l].
+    # 4) Reverse the sub-array nums[k + 1:].
+    #
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
